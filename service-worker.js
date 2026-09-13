520daa31a00a861f09ae4ac464945dfeccef226f
walterRigoni <worfnono@gmail.com>
2026-09-13 19:07:43 -0300

    Hospitalito v236: refresh PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='hospitalito-v232';
+const CACHE='hospitalito-v236';
 const CORE=['./','./index.html','./manifest.webmanifest','./hospitalito-icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
